3eb38cf4085edc812cac86574ecc7aa5752c066b
pinn-tejasens <pinntejasen@Pinns-Mac.local>
2014-06-06 01:46:44 -0700

    initial

---
+++ testing/testingTests/testingTests.m
@@ -0,0 +1,34 @@
+//
+//  testingTests.m
+//  testingTests
+//
+//  Created by Pinn Tejasen on 6/6/2557 BE.
+//  Copyright (c) 2557 Pinn Tejasen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testingTests : XCTestCase
+
+@end
+
+@implementation testingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
