4cde35ed32de6216f5f26a00b83626fe517f7ce5
pinn-tejasens <pinntejasen@Pinns-Mac.local>
2014-06-06 01:58:26 -0700

    Initial commit from local git

---
+++ jSonTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  jSonTest
+//
+//  Created by Supasit on 5/9/57 BE.
+//  Copyright (c) 2557 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
